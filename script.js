acfa0bd9db39ecc91bb3534628791674297e9ce0
John Dev <joaoguilhermeofc.contato@gmail.com>
2022-01-13 16:42:56 -0300

    Secound Commit :bookmark:

--- script.js
+++ script.js
@@ -1,6 +1,44 @@
 function verificar() {
     var data = new Date();
-    var ano = data.getFullYear();
+    var ano = data.getFullYear(); //full year pega os 4 digitos do ano por exemplo 2 0 2 1
     var nascimento = document.getElementById('txtano');
-    //if()
+    var res = document.querySelector('div#res')
+    if (nascimento.value.length == 0 || Number(nascimento.value) > ano) {
+        window.alert('[ERRO] Verifique os dados e tente novamente');
+    } else {
+        var sexo = document.getElementsByName('radsex');
+        var idade = ano - Number(nascimento.value);
+        var genero = '';
+        var img = document.createElement('img');
+        img.setAttribute('id', 'foto');
+        if (sexo[0].checked) {
+            genero = 'homem';
+            if (idade >= 0 && idade < 10) {
+                img.setAttribute('src', 'bebem.png');
+            }
+            else if (idade < 21) {
+                img.setAttribute('src', 'jovemm.png');
+            }
+            else if (idade < 50) {
+                img.setAttribute('src', 'adultom.png');
+            } else {
+                img.setAttribute('src', 'idosom.png');
+            }
+        } else if (sexo[1].checked) {
+            genero = 'mulher';
+            if (idade >= 0 && idade < 10) {
+                img.setAttribute('src', 'bebef.png');
+            }
+            else if (idade < 21) {
+                img.setAttribute('src', 'jovemf.png');
+            }
+            else if (idade < 50) {
+                img.setAttribute('src', 'adultof.png');
+            } else {
+                img.setAttribute('src', 'idosaf.png');
+            }
+        }
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos`;
+        res.appendChild(img);
+    }
 }
